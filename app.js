a83be01226d8bfc1e2a8ea9b2d942e435c704537
bonox2 <fontoften@gmail.com>
2021-04-13 11:38:01 -0400

    not working servers data

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-let covidData = {}
+let covidData = []
 const today = new Date().toJSON().split('T')[0]
 getCovidData(`https://api-covid19.rnbo.gov.ua/data?to=${today}`)
 
@@ -16,64 +16,49 @@ async function getCovidData(url) {
     }
 }
 
-function addMessages(elem, messages) {
-    let messagesHtml = '' //переменная равна строке
-    messages.forEach(message => {//функцыя для каждого елемента
-        messagesHtml += renderMessage(message)//messagesHtml добавляем renderMessage
+
+
+
+function addCountryData(elem, covidData) {
+    let covidDataHtml = '' //переменная равна строке
+    covidData.forEach(country => {
+      covidDataHtml += renderCountry(country)
     });
-    elem.innerHTML = messagesHtml//приравниваем
+    elem.innerHTML = covidDataHtml//приравниваем
 }
-/*{
-"id": 32893,
-"label": {
-"en": "Kazakhstan",
-"uk": "Казахстан"
-},
-"country": "Kazakhstan",
-"confirmed": 307676,
-"deaths": 3249,
-"recovered": 275505,
-"existing": 28922,
-"suspicion": 0,
-"lat": 48.0196,
-"lng": 66.9237,
-"delta_confirmed": 1816,
-"delta_deaths": 7,
-"delta_recovered": 1293,
-"delta_existing": 516,
-"delta_suspicion": 0
-},*/
-function renderMessage(data) {//каркас секции
+addCountryData()
+
+
+
+function renderCountry(data) {
     const html =
         `<table class="dashboard-content" id="dashboardContent">
         <thead>
-          <th>${data.country}</th>
-          <th>${data.confirmed}</th>
-          <th>${data.Deaths}</th>
-          <th>${data.Recovered}</th>
-          <th>${data.Existing}</th>
+          <th>}</th>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
         </thead>
         <tbody>
           <tr>
-            <td>usa</td>
+            <td>${data.country}</td>
             <td class="confirmed">
-              <div>confirmed</div>
-              <div>confirmed delta</div>
+              <div>${data.confirmed}</div>
+              <div>${data.delta_confirmed}</div>
             </td>
             <td class="deaths">
-              <div>death</div>
-              <div>death delta</div>
+              <div>${data.deaths}</div>
+              <div>${data.delta_deaths}</div>
             </td>
             <td class="recovered">
-              <div>recover</div>
-              <div>recover delta</div>
+              <div>${data.recovered}</div>
+              <div>${data.delta_recovered}</div>
             </td>
             <td class="existing">
-              <div>exist</div>
-              <div>exist delta</div>
+              <div>${data.existing}</div>
+              <div>${data.delta_existing}</div>
             </td>
-          </tr>
-        </tbody>
-      </table>
-    return html//возвращаем результат
+          </tr>`
+    return html
 }
